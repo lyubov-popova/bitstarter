84484cebd0a5fb94e0ace42cafaa4e0f0f113b1d
Ubuntu <ubuntu@ip-172-31-22-93.us-west-2.compute.internal>
2013-07-09 22:10:16 +0000

    Changed web.js to read from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var buffer = fs.readFileSync('/home/ubuntu/node-js-sample.git/bitstarter/index.html');
+  var text = buffer.toString(); 
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
